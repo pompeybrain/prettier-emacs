cae21048b8b99327df6d0bb4c6d440ce21bc5636
pompeybrain <pompeybrain@gmail.com>
2019-07-25 23:55:16 +0800

    add js-mode support

--- prettier.el
+++ prettier.el
@@ -11,55 +11,12 @@
 
 ;;; Code:
 
-(defun prettier-sync-process (command args input-buffer
-                                      &optional success-code output-handler silent error-handler)
-  "call-process wraper : excute command with friendly output and error handle."
-  (let ((input-file (make-temp-file (file-name-nondirectory command) nil "-input"))
-        (output-buffer (get-buffer-create (concat (file-name-nondirectory command) "-output")))
-        (output-string nil)
-        (res-code nil)
-        (error-file (make-temp-file (file-name-nondirectory command)))
-        (error-buffer (get-buffer-create (concat (file-name-nondirectory command) "-error")))
-        (error-output nil))
-    (unwind-protect
-        (progn
-          (if input-buffer
-              (with-current-buffer input-buffer
-                (save-restriction
-                  (widen)
-                  (write-region nil nil input-file)))
-            (setq input-file nil))
-          (setq res-code (apply 'call-process command input-file (list output-buffer error-file)
-                                nil args))
-          (with-current-buffer output-buffer
-            (setq output-string (buffer-string)))
-          (with-current-buffer error-buffer
-            (erase-buffer)
-            (insert-file-contents error-file)
-            (setq error-output (buffer-string)))
-          (unless (string-empty-p error-output)
-            (when error-handler
-              (funcall error-handler error-output)))
-          (if (member res-code success-code)
-              (progn
-                (unless (or silent (string-empty-p error-output))
-                  (message "command [%s] succeeded but has warn:\n %s" command error-output))
-                (if output-handler
-                    (funcall output-handler output-string)
-                  output-string))
-            (unless silent (message "command [%s] failed:\n %s" command error-output))
-            ""))
-      (kill-buffer error-buffer)
-      (when input-file (delete-file input-file))
-      (delete-file error-file)
-      (kill-buffer output-buffer))))
-
 (defvar prettier-support-modes
-  '(js2-mode javascript-mode css-mode scss-mode less-mode json-mode web-mode
+  '(js2-mode js-mode css-mode scss-mode less-mode json-mode web-mode
              yaml-mode typescript-mode markdown-mode) "prettier support major mode")
 
 (defvar prettier-support-modes-alist
-  '((js2-mode . "babylon") (javascript-mode . "babylon") (css-mode . "css")
+  '((js2-mode . "babylon") (js-mode . "babylon") (css-mode . "css")
     (scss-mode . "scss") (less-mode . "less") (json-mode . "json") (web-mode . "vue") (yaml-mode . "yaml")
     (typescript-mode . "typescript") (markdown-mode . "markdown"))
   "prettier support major mode parser alist")
@@ -322,6 +279,49 @@
              (prettier-add-save-format-hook))
     (remove-hook 'after-change-major-mode-hook #'prettier-add-save-format-hook)
     (remove-hook 'before-save-hook 'prettier-format 'local)))
+
+(defun prettier-sync-process (command args input-buffer
+                                      &optional success-code output-handler silent error-handler)
+  "call-process wraper : excute command with friendly output and error handle."
+  (let ((input-file (make-temp-file (file-name-nondirectory command) nil "-input"))
+        (output-buffer (get-buffer-create (concat (file-name-nondirectory command) "-output")))
+        (output-string nil)
+        (res-code nil)
+        (error-file (make-temp-file (file-name-nondirectory command)))
+        (error-buffer (get-buffer-create (concat (file-name-nondirectory command) "-error")))
+        (error-output nil))
+    (unwind-protect
+        (progn
+          (if input-buffer
+              (with-current-buffer input-buffer
+                (save-restriction
+                  (widen)
+                  (write-region nil nil input-file)))
+            (setq input-file nil))
+          (setq res-code (apply 'call-process command input-file (list output-buffer error-file)
+                                nil args))
+          (with-current-buffer output-buffer
+            (setq output-string (buffer-string)))
+          (with-current-buffer error-buffer
+            (erase-buffer)
+            (insert-file-contents error-file)
+            (setq error-output (buffer-string)))
+          (unless (string-empty-p error-output)
+            (when error-handler
+              (funcall error-handler error-output)))
+          (if (member res-code success-code)
+              (progn
+                (unless (or silent (string-empty-p error-output))
+                  (message "command [%s] succeeded but has warn:\n %s" command error-output))
+                (if output-handler
+                    (funcall output-handler output-string)
+                  output-string))
+            (unless silent (message "command [%s] failed:\n %s" command error-output))
+            ""))
+      (kill-buffer error-buffer)
+      (when input-file (delete-file input-file))
+      (delete-file error-file)
+      (kill-buffer output-buffer))))
 
 (provide 'prettier)
 ;;; prettier.el ends here
